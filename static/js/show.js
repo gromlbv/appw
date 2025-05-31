c8c48ae056a0865872ab9b8c4ae41e69cedc48f9
lbv_o <gromlbv@gmail.com>
2025-05-31 21:21:35 +0300

    nav animation added

---
+++ static/js/show.js
@@ -0,0 +1,11 @@
+$(document).ready(function() {
+
+    // OPEN HEADER NAVBAR
+
+    $("#show-nav, #nav-bg").click(function() {
+        $("#show-nav").toggleClass("active");
+        $("#nav-bg").toggleClass("active");
+        $("#nav").toggleClass("visible");
+    });
+
+});
