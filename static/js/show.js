9313d571a1f25db431b76226f2ea8cf485454a1d
lbv_o <gromlbv@gmail.com>
2025-07-02 20:41:09 +0300

    update 0.17

--- static/js/show.js
+++ static/js/show.js
@@ -5,34 +5,36 @@ $(document).ready(function () {
         $("#nav").toggleClass("visible");
         $('body').toggleClass('no-scroll');
     });
-});
 
-
-const modal = $('#gameModal');
-
-if (modal.data('linked-app')) {
-    modal.css('display', 'flex');
-}
-
-$(window).on('click', function (e) {
-    if (e.target.id === 'gameModal') closeModal();
-});
-
-$(window).on('keydown', function (e) {
-    if (e.key === 'Escape') {
-        e.preventDefault();
-        e.stopPropagation();
-        closeModal();
-    }
-});
-
-function closeModal() {
-    $('#gameModal').hide();
-    document.body.classList.remove('no-scroll');
+    
+    const modal = $('#gameModal');
 
     if (modal.data('linked-app')) {
-        window.location.href = 'https://appw.su/';
-    } else {
-        history.pushState(null, '', '/');
+        modal.css('display', 'flex');
     }
-}
+
+    $(window).on('click', function (e) {
+        if (e.target.id === 'gameModal') closeModal();
+    });
+
+    $(window).on('keydown', function (e) {
+        if (e.key === 'Escape') {
+            e.preventDefault();
+            e.stopPropagation();
+            closeModal();
+        }
+    });
+
+
+    function closeModal() {
+        $('#gameModal').hide();
+        document.body.classList.remove('no-scroll');
+
+        if (modal.data('linked-app')) {
+            window.location.href = 'https://appw.su/';
+        } else {
+            history.pushState(null, '', '/');
+        }
+    }
+});
+
